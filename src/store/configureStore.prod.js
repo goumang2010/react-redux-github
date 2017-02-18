1ebd3af4453e16084f58019b0945079189dc2b31
goumang2010 <goumang2010@live.com>
2017-02-18 15:25:53 +0800

    prod script

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -2,14 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 // import Thunk from 'redux-thunk';
 import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
-import { rootReducer } from 'root';
-import promiseMiddleware from 'middleware/promiseMiddleware';
+import { rootReducer, rootEpic } from 'root';
+import { createEpicMiddleware } from 'redux-observable';
 
-const routeMiddleware = routerMiddleware(hashHistory);
+const routeMiddleware = routerMiddleware(browserHistory);
+const epicMiddleware = createEpicMiddleware(rootEpic);
 
 const middleware = [
     // Thunk,
-    routeMiddleware
+    routeMiddleware,
+    epicMiddleware
 ];
 
 const enhancer = applyMiddleware(...middleware);
